568a34b455065bf219eff87e192fb2d9612d60d1
piratecarrot <39475419+piratecarrot@users.noreply.github.com>
2019-08-12 09:33:26 +1000

    More work

--- optris.py
+++ optris.py
@@ -1,6 +1,8 @@
-from ctypes import CDLL, c_int, c_float, byref
+import ctypes
+import numpy
+import matplotlib.pyplot as plt
 
-lib = CDLL('.\\libirimager.dll')
+lib = ctypes.CDLL('.\\libirimager.dll')
 
 def usb_init(xml_config: str, formats_def: str = None, log_file: str = None) -> int:
     return lib.evo_irimager_usb_init(xml_config.encode(), None if formats_def is None else formats_def.encode(), None if log_file is None else log_file.encode())
@@ -12,22 +14,47 @@ def terminate() -> int:
     return lib.evo_irimager_terminate(None)
 
 def get_thermal_image_size() -> (int, int):
-    width = c_int()
-    height = c_int()
-    _ = lib.evo_irimager_get_thermal_image_size(byref(width), byref(height))
+    width = ctypes.c_int()
+    height = ctypes.c_int()
+    _ = lib.evo_irimager_get_thermal_image_size(ctypes.byref(width), ctypes.byref(height))
     return width.value, height.value
 
 def get_palette_image_size() -> (int, int):
-    width = c_int()
-    height = c_int()
-    _ = lib.evo_irimager_get_palette_image_size(byref(width), byref(height))
+    width = ctypes.c_int()
+    height = ctypes.c_int()
+    _ = lib.evo_irimager_get_palette_image_size(ctypes.byref(width), ctypes.byref(height))
     return width.value, height.value
 
 # __IRDIRECTSDK_API__ int evo_irimager_get_thermal_image(int* w, int* h, unsigned short* data);
+def get_thermal_image(width: int, height: int) -> numpy.ndarray:
+    w = ctypes.byref(ctypes.c_int(width))
+    h = ctypes.byref(ctypes.c_int(height))
+    thermalData = numpy.empty((height, width), dtype=numpy.uint16)
+    thermalDataPointer = thermalData.ctypes.data_as(ctypes.POINTER(ctypes.c_ushort))
+    _ = lib.evo_irimager_get_thermal_image(w, h, thermalDataPointer)
+    return thermalData
 
 # __IRDIRECTSDK_API__ int evo_irimager_get_palette_image(int* w, int* h, unsigned char* data);
+def get_palette_image(width: int, height: int) -> numpy.ndarray:
+    w = ctypes.byref(ctypes.c_int(width))
+    h = ctypes.byref(ctypes.c_int(height))
+    paletteData = numpy.empty((height, width, 3), dtype=numpy.uint8)
+    paletteDataPointer = paletteData.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte))
+    retVal = -1
+    while retVal != 0:
+        retVal = lib.evo_irimager_get_palette_image(w, h, paletteDataPointer)
+    return paletteData
 
 # __IRDIRECTSDK_API__ int evo_irimager_get_thermal_palette_image(int w_t, int h_t, unsigned short* data_t, int w_p, int h_p, unsigned char* data_p );
+def get_thermal_palette_image(width: int, height: int) -> (numpy.ndarray, numpy.ndarray):
+    w = ctypes.byref(ctypes.c_int(width))
+    h = ctypes.byref(ctypes.c_int(height))
+    thermalData = numpy.empty((height, width), dtype=numpy.uint16)
+    paletteData = numpy.empty((height, width, 3), dtype=numpy.uint8)
+    thermalDataPointer = thermalData.ctypes.data_as(ctypes.POINTER(ctypes.c_ushort))
+    paletteDataPointer = paletteData.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte))
+    _ = lib.evo_irimager_get_palette_image(w, h, thermalDataPointer, w, h, paletteDataPointer)
+    return (thermalData, paletteData)
 
 def set_palette(id: int) -> int:
     return lib.evo_irimager_set_palette(id)
@@ -49,12 +76,12 @@ def set_radiation_parameters(emissivity: float, transmissivity: float, ambientTe
 
 # __IRDIRECTSDK_API__ int evo_irimager_to_palette_save_png(unsigned short* thermal_data, int w, int h, const char* path, int palette, int palette_scale);
 
-def set_focusmotor_pos(position: float) -> int:
+def set_focus_motor_position(position: float) -> int:
     return lib.evo_irimager_set_focusmotor_pos(position)
 
-def get_focusmotor_pos() -> float:
-    position = c_float()
-    _ = lib.evo_irimager_get_focusmotor_pos(byref(position))
+def get_focus_motor_position() -> float:
+    position = ctypes.c_float()
+    _ = lib.evo_irimager_get_focusmotor_pos(ctypes.byref(position))
     return position.value
 
 def daemon_launch() -> int:
@@ -66,8 +93,15 @@ def daemon_is_running() -> int:
 def daemon_kill() -> int:
     return lib.evo_irimager_daemon_kill(None)
 
-usb_init('.\\generic.xml')
-w, h = get_thermal_image_size()
-print('{} x {}'.format(w, h))
+if __name__ == "__main__":
+    usb_init('.\\generic.xml')
 
-terminate()
+    set_palette(1)
+    w, h = get_thermal_image_size()
+    print('{} x {}'.format(w, h))
+
+    frame = get_palette_image(w, h)
+    plt.imshow(frame)
+    plt.show()
+
+    terminate()
